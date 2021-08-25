25939b5f4e724087adaf2fdfce9ce85c169d2d22
Nishu Tyagi <tyaginishu7@gmail.com>
2021-08-25 22:31:17 +0530

    Refactored:Create Table of address_book

--- address_book.sql
+++ address_book.sql
@@ -2,3 +2,15 @@ CREATE DATABASE address_book;
 show databases;
 USE address_book;
 SELECT DATABASE();
+
+CREATE TABLE address_book (
+	first_name				VARCHAR(255) NOT NULL,
+    last_name				VARCHAR(255) NOT NULL,
+    address					VARCHAR(255) NOT NULL,
+    city					VARCHAR(255) NOT NULL,
+    state					VARCHAR(255) NOT NULL,
+    zip						INT(6) NOT NULL,
+    phone_number			VARCHAR(10) NOT NULL,
+    email_id				VARCHAR(255) NOT NULL
+);
+DESCRIBE address_book;
